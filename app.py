7094f49b47cfa08c09fbd1eabe8bd74ec682db9c
zt07 <zaheeb07@gmail.com>
2022-07-29 22:39:46 -0400

    Fully works! prolly the final commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from password import generate_password
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    length = int(request.form['length'])
+    password = generate_password(length)
+    
+    return render_template('index.html', password=password)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ password.py
@@ -0,0 +1,16 @@
+from english_words import english_words_set as words
+from random import choice
+
+
+words = [word for word in words if len(word) <= 5]
+
+
+
+# Generate a random password of the given length
+def generate_password(length):
+    password = ''
+    for i in range(length):
+        password += choice(words)
+        password+= ' '
+    return password
+
